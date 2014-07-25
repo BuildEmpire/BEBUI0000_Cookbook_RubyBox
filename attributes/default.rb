ea80ef76fd468b87ece9dc693b8b92c90ec2e88b
Fred Thompson <fred.thompson@buildempire.co.uk>
2014-07-25 09:56:33 +0100

    Added the default version back in.

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,4 +1,4 @@
 default['cookbook_rubybox']['ruby']['use_rvm'] = true
-default['cookbook_rubybox']['ruby']['versions'] = %w()
+default['cookbook_rubybox']['ruby']['versions'] = %w(1.9.3-p429)
 default['cookbook_rubybox']['ruby']['global_version'] = '1.9.3-p429'
 default['build-essential']['compile_time'] = true
